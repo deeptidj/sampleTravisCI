44fc3c875d678b85806da6ea332fb65547fba81f
Deepti Jain <deeptidj@gmail.com>
2018-02-28 14:29:35 -0800

    adding another print method

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -5,6 +5,11 @@ public class HelloWorld {
 
     public static void main(String args[]) {
             System.out.println("Hello, World");
+            printAgain("Hello, World");
+        }
+
+        public static void printAgain(String str) {
+            System.out.println(str);
         }
 
 
